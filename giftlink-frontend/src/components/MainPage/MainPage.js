aea5f6dc9ac5313ff4a70aded7a9f12544b16cb2
Matthieu Simon <matthieu10.simon@laposte.net>
2025-04-04 14:03:49 -0600

    Implement front-end of main page and navigation bar

--- giftlink-frontend/src/components/MainPage/MainPage.js
+++ giftlink-frontend/src/components/MainPage/MainPage.js
@@ -8,18 +8,22 @@ function MainPage() {
 
     useEffect(() => {
         // Task 1: Write async fetch operation
-        // Write your code below this line
+        const fetchGifts = async () => fetch(urlConfig.backendUrl + "/api/gifts").then(r => r.json());
+        fetchGifts()
+            .then(data => setGifts(data))
+            .catch(console.error);
     }, []);
 
     // Task 2: Navigate to details page
     const goToDetailsPage = (productId) => {
         // Write your code below this line
-
+        navigate(`/app/product/${productId}`);
       };
 
     // Task 3: Format timestamp
     const formatDate = (timestamp) => {
         // Write your code below this line
+        return new Date(timestamp).toLocaleDateString()
       };
 
     const getConditionClass = (condition) => {
@@ -34,19 +38,19 @@ function MainPage() {
                         <div className="card product-card">
 
                             {/* // Task 4: Display gift image or placeholder */}
-                            {/* // Write your code below this line */}
+                            <img className='card-img-top' src={gift.image} />
 
                             <div className="card-body">
 
-                                {/* // Task 5: Display gift image or placeholder */}
-                                {/* // Write your code below this line */}
+                                {/* // Task 5: Display gift name */}
+                                <h5 className="card-title">{gift.name}</h5>
 
                                 <p className={`card-text ${getConditionClass(gift.condition)}`}>
                                 {gift.condition}
                                 </p>
 
                                 {/* // Task 6: Display gift image or placeholder */}
-                                {/* // Write your code below this line */}
+                                <p className="card-text">{formatDate(gift.date_added)}</p>
                                 
 
                                 <button onClick={() => goToDetailsPage(gift.id)} className="btn btn-primary">
