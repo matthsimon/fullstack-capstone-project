8be96a54e66dd09c8ba18ea7aa53e5a8bf29d9a0
Matthieu Simon <matthieu10.simon@laposte.net>
2025-04-05 18:41:49 -0600

    Implement product detail page frontend

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import LoginPage from './components/LoginPage/LoginPage';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
           {/* the final code will not pass the products to every page, but each page will call the server API */}
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
+          <Route path="/app/product/:productId" element={<DetailsPage />} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/login" element={<LoginPage />} />
         </Routes>
