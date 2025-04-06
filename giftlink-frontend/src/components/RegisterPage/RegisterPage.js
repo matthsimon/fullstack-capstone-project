d9d796283b7d2567924358095bfd0d33e484739f
Matthieu Simon <matthieu10.simon@laposte.net>
2025-04-05 20:53:10 -0600

    Use bootstrap to style the register page

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -23,27 +23,27 @@ function RegisterPage() {
                 <div className="register-card p-4 border rounded">
                     <h2 className="text-center mb-4 font-weight-bold">Register</h2>
                     <form>
-                        <div className="form-group">
+                        <div className="form-group mt-2 mb-2">
                             <label htmlFor="firstName">First Name</label>
                             <input type="text" className="form-control" id="firstName" placeholder='Enter your first name' 
                                 value={firstName} onChange={(e) => setFirstName(e.target.value)} />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group mt-2 mb-2">
                             <label htmlFor="lastName">Last Name</label>
                             <input type="text" className="form-control" id="lastName" placeholder='Enter your last name'
                                     value={lastName} onChange={(e) => setLastName(e.target.value)}/>
                         </div>
-                        <div className="form-group">
+                        <div className="form-group mt-2 mb-2">
                             <label htmlFor="email">Email</label>
                             <input type="email" className="form-control" id="email" placeholder='Enter your email' 
                                    value={email} onChange={(e) => setEmail(e.target.value)} />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group mt-2 mb-2">
                             <label htmlFor="password">Password</label>
                             <input type="password" className="form-control" id="password" placeholder='Enter your password' 
                                     value={password} onChange={(e) => setPassword(e.target.value)} />
                         </div>
-                        <button className="btn btn-primary" onClick={handleRegister}>Register</button>
+                        <button className="btn btn-primary mt-4" onClick={handleRegister}>Register</button>
                     </form>
                     <p className="mt-4 text-center">
                         Already a member? <a href="/app/login" className="text-primary">Login</a>
